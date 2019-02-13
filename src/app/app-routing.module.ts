ffdfe0e57490b6ec31616e225fc5acfe0b2ce4b5
kankor2 <i.techology313@gmail.com>
2019-02-13 14:54:37 -0700

    add azmon page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'azmon', loadChildren: './azmon/azmon.module#AzmonPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/azmon/azmon.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { AzmonPage } from './azmon.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AzmonPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [AzmonPage]
+})
+export class AzmonPageModule {}
